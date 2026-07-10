c052b6f1e7753d7c0494fed99e953c361863c3b4
Eddie Wafula <eddywafula93@gmail.com>
2026-07-10 15:55:02 +0300

    Sprint 3 - Customize AdminLTE branding and navigation

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,28 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Tax Compliance & Reconciliation System</h1>
+@stop
+
+@section('content')
+
+<div class="row">
+
+    <div class="col-lg-3 col-6">
+        <div class="small-box bg-info">
+            <div class="inner">
+                <h3>0</h3>
+                <p>Clients</p>
+            </div>
+
+            <div class="icon">
+                <i class="fas fa-users"></i>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+@stop
--- routes/web.php
+++ routes/web.php
@@ -9,9 +9,8 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('admin.dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware(['auth'])->group(function () {
 
     // Client Routes
