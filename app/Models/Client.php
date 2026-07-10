b167d2c6abedd0bb7b251a73c98d8dde0b4775f5
Eddie Wafula <eddywafula93@gmail.com>
2026-07-10 15:10:59 +0300

    Sprint 1 - Laravel setup, authentication, roles and client module foundation

---
+++ app/Models/Client.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Client extends Model
+{
+    //
+    protected $fillable = [
+
+    'company_name',
+
+    'pin',
+
+    'email',
+
+    'phone',
+
+    'contact_person',
+
+    'industry',
+
+    'address',
+
+    'is_active',
+
+];
+}
---
+++ resources/views/clients/index.blade.php
@@ -0,0 +1,13 @@
+<x-app-layout>
+
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl">
+            Clients
+        </h2>
+    </x-slot>
+
+    <div class="p-6">
+        Welcome to the Client Module.
+    </div>
+
+</x-app-layout>
